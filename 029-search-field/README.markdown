4ba3893255f9d82dfee0c1d2544d0c48bd5bf290
vinod2305 <42367725+vinod2305@users.noreply.github.com>
2019-09-07 17:48:16 +0530

    Add files via upload

---
+++ 029-search-field/README.markdown
@@ -0,0 +1,4 @@
+# Search field-029
+ _A Pen created at CodePen.io. Original URL: [https://codepen.io/vinod2305/pen/OJLzKeG](https://codepen.io/vinod2305/pen/OJLzKeG).
+
+ 
---
+++ 029-search-field/src/script.js
@@ -0,0 +1,18 @@
+var searchfield=document.querySelector(".searchfield");
+var suggestions=document.querySelector(".suggestions");
+
+searchfield.addEventListener("keyup", function(){
+  var text=searchfield.value;
+  if(text.length){
+    suggestions.innerHTML="";
+    suggestions.classList.add("active");
+    suggestions.innerHTML+="<li><b>"+text+"</b></li>";
+    suggestions.innerHTML+="<li> Veritatis et <b>"+text+" </b></li>";
+    suggestions.innerHTML+="<li> ut aliquid ex <b>"+text+" </b>vero eos</li>";
+  }else{
+    suggestions.innerHTML="";
+    suggestions.classList.remove("active");
+  }
+});
+
+
