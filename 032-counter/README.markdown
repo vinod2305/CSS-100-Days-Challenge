74f6e51b960e324344e4ff94d884c7277cf52687
vinod2305 <42367725+vinod2305@users.noreply.github.com>
2019-09-10 10:44:57 +0530

    Add files via upload

---
+++ 032-counter/README.markdown
@@ -0,0 +1,4 @@
+# Counter-032
+ _A Pen created at CodePen.io. Original URL: [https://codepen.io/vinod2305/pen/PoYRbBr](https://codepen.io/vinod2305/pen/PoYRbBr).
+
+ 
---
+++ 032-counter/dist/script.js
@@ -0,0 +1,37 @@
+var number=  document.querySelector('.number')
+var a=0;
+
+document.querySelector('.minus').addEventListener('click', function(){
+  updateNumber(-1);
+  console.log("a");
+})
+
+document.querySelector('.plus').addEventListener('click', function(){
+  updateNumber(1);
+  console.log(number);
+})
+
+function updateNumber(value){
+  a+= value;
+  var x= number.getElementsByTagName('span');
+  console.log(number.getElementsByTagName('span'));
+
+  x.className= '';
+   
+  if (value > 0) { 
+		x[x.length-1].insertAdjacentHTML("afterend", '<span class="fadeInDown">' + a + '</span>');
+    x[x.length-2].classList=('fadeOutUp');
+    x[x.length-1].classList=('fadeInUp');
+    for(var i=0;i<nx.length-1;i++){
+      x[i].style.display="none";
+    }
+	} else { // fadeDown Animation
+  x[x.length-1].insertAdjacentHTML("afterend", '<span class="fadeInDown">' + a + '</span>');
+    x[x.length-2].classList=('fadeOutDown');
+    x[x.length-1].classList=('fadeInDown');
+    for(var i=0;i<x.length-1;i++){
+      x[i].style.display="none";
+    }
+	}
+  
+}
